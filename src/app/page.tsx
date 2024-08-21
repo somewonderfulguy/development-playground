85c465598688cb68e539141e2b7c5eb70f06e6d8
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-08-21 19:00:32 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="p-5">
+      <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
+    </main>
+  )
+}
