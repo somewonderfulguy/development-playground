bfda10150f8a111ee5b7a45dff992b08e62dce5a
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-08-28 22:59:04 +0300

    basic ChatGPT chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import ClientComponent from './ClientComponent'
+import Chat from '@/app/components/Chat'
+import { Separator } from '@radix-ui/react-dropdown-menu'
+import { auth } from '@/auth'
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Welcome To GPT Chat</h1>
-      <ClientComponent />
+      {!session?.user?.email ? (
+        <div>You need to log in to use this chat.</div>
+      ) : (
+        <>
+          <Separator className="my-5" />
+          <Chat />
+        </>
+      )}
     </main>
   )
 }
---
+++ src/server-actions/getCompletion.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import OpenAI from 'openai'
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+})
+
+export async function getCompletion(
+  messageHistory: { role: 'user' | 'assistant'; content: string }[]
+) {
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    messages: messageHistory
+  })
+
+  const messages = [
+    ...messageHistory,
+    response.choices[0].message as {
+      role: 'user' | 'assistant'
+      content: string
+    }
+  ]
+
+  return { messages }
+}
