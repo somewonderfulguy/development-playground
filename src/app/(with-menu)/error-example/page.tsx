9eb79c73b24a48a4f19063f33827d7e46ca665a9
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-09-14 00:23:36 +0300

    fix build error because of error example component

--- src/app/(with-menu)/error-example/page.tsx
+++ src/app/(with-menu)/error-example/page.tsx
@@ -1,5 +1,15 @@
+'use client'
+
+import { useEffect } from 'react'
+
 export default function ErrorExample() {
   const undef = undefined as any
-  const newVariable = undef.map((x: any) => x.variable)
+
+  let newVariable: string[] = []
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    newVariable = undef.map((x: any) => x.variable)
+  }, [undef])
+
   return <div>Content of the page does not matter - it will crash anyway. {newVariable}</div>
 }
