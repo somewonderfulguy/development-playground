63f68ae2517a14b3acb2a448687baf4ce2bc65f7
Dmitriy Yastrebov <somewonderfulguy@gmail.com>
2024-10-01 17:37:23 +0300

    menu updates - show smth for prod & smth for dev; fix active items in menu

--- src/components/ApplicationLayout/ApplicationLayout.tsx
+++ src/components/ApplicationLayout/ApplicationLayout.tsx
@@ -1,22 +1,18 @@
 'use client'
 
 import { useState, ReactNode } from 'react'
-import { useParams } from 'next/navigation'
-import Locale from 'intl-locale-textinfo-polyfill'
 
 import { TooltipProvider } from '@/components/ui/tooltip'
 import AppControls from './components/AppControls'
 import AppNav from './components/AppNav'
+import { useIsRtl } from '@/locales/client'
 
 type Props = {
   children: ReactNode
 }
 
 export default function ApplicationLayout({ children }: Props) {
-  const { locale } = useParams()
-  // TODO: move to reusable utility (or hook)
-  const { direction: dir } = new Locale(String(locale)).textInfo
-  const isRTL = dir === 'rtl'
+  const isRtl = useIsRtl()
 
   const [isDrawerOpen, setIsDrawerOpen] = useState(true)
   const [isDarkTheme, setIsDarkTheme] = useState(false)
@@ -34,14 +30,14 @@ export default function ApplicationLayout({ children }: Props) {
           {/* Drawer */}
           <div
             className={`${isDrawerOpen ? 'translate-x-0' : '-translate-x-full'} fixed top-0 ${
-              isRTL ? 'right-0' : 'left-0'
+              isRtl ? 'right-0' : 'left-0'
             } h-full w-[270px] bg-secondary p-4 pt-18 transition-transform duration-300 ease-in-out`}
           >
             <AppNav />
           </div>
           {/* Main Content */}
           <div
-            className={`flex-grow overflow-y-auto transition-all duration-300 ease-in-out ${isDrawerOpen ? (isRTL ? 'mr-[270px]' : 'ml-[270px]') : 'ml-0'} `}
+            className={`flex-grow overflow-y-auto transition-all duration-300 ease-in-out ${isDrawerOpen ? (isRtl ? 'mr-[270px]' : 'ml-[270px]') : 'ml-0'} `}
           >
             {children}
           </div>
--- src/components/ApplicationLayout/components/AppNav/AppNav.tsx
+++ src/components/ApplicationLayout/components/AppNav/AppNav.tsx
@@ -7,52 +7,51 @@ import {
   ChartBarSquareIcon,
   PresentationChartBarIcon,
   HomeIcon,
-  QuestionMarkCircleIcon,
-  ExclamationCircleIcon,
   WindowIcon
 } from '@heroicons/react/24/outline'
-import {
-  LayoutDashboardIcon,
-  MessageSquareIcon,
-  ShieldIcon,
-  InfoIcon,
-  ChevronDownIcon,
-  ChevronRightIcon
-} from 'lucide-react'
 import { BiFootball } from 'react-icons/bi'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { Fragment, useState } from 'react'
-import clsx from 'clsx'
-import { useScopedI18n } from '@/locales/client'
+import { useState } from 'react'
+
+import { useCurrentLocale, useIsRtl, useScopedI18n } from '@/locales/client'
+import { isDevelopment } from '@/constants/common'
 
 const useLinks = () => {
   const t = useScopedI18n('nav')
 
   return [
-    {
-      name: t('home'),
-      href: '/',
-      icon: HomeIcon
-    },
-    {
-      name: t('dashboard'),
-      icon: PresentationChartBarIcon,
-      children: [
-        {
-          name: t('dashboard.main'),
-          href: '/dashboard',
-          icon: ChartBarSquareIcon
-        },
-        {
-          name: t('dashboard.invoices'),
-          href: '/dashboard/invoices',
-          icon: DocumentDuplicateIcon
-        },
-        { name: t('dashboard.customers'), href: '/dashboard/customers', icon: UserGroupIcon }
-      ]
-    },
-    { name: t('chatGPT'), href: '/chat-gpt', icon: ChatBubbleLeftRightIcon },
+    ...(isDevelopment
+      ? [
+          {
+            name: t('home'),
+            href: '/',
+            icon: HomeIcon
+          }
+        ]
+      : []),
+    ...(isDevelopment
+      ? [
+          {
+            name: t('dashboard'),
+            icon: PresentationChartBarIcon,
+            children: [
+              {
+                name: t('dashboard.main'),
+                href: '/dashboard',
+                icon: ChartBarSquareIcon
+              },
+              {
+                name: t('dashboard.invoices'),
+                href: '/dashboard/invoices',
+                icon: DocumentDuplicateIcon
+              },
+              { name: t('dashboard.customers'), href: '/dashboard/customers', icon: UserGroupIcon }
+            ]
+          }
+        ]
+      : []),
+    ...(isDevelopment ? [{ name: t('chatGPT'), href: '/chat-gpt', icon: ChatBubbleLeftRightIcon }] : []),
     { name: t('fcClubs'), href: '/fc-clubs', icon: BiFootball },
     { name: t('about'), href: '/about', icon: WindowIcon }
   ]
@@ -60,6 +59,9 @@ const useLinks = () => {
 
 export default function AppNav() {
   const pathname = usePathname()
+  const currentLocale = useCurrentLocale()
+
+  const isRtl = useIsRtl()
 
   const links = useLinks()
 
@@ -78,27 +80,26 @@ export default function AppNav() {
           const item = (
             <>
               <LinkIcon className="w-6" size="1.8em" />
-              <p className="hidden md:block">{link.name}</p>
+              <p>{link.name}</p>
             </>
           )
-          const itemClassName =
-            'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 hover:bg-sky-100 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3'
-          const childrenHrefs = link.children?.map((child) => child.href) || []
+          const withLocale = (href: string) => `/${currentLocale}${href}`
+          const childrenHrefs = link.children?.map((child) => withLocale(child.href)) || []
+
           return (
             <li key={idx}>
               {link.href ? (
                 <Link
                   href={link.href}
                   className={`${
-                    pathname === link.href && 'bg-primary/[0.05] text-primary'
+                    pathname === withLocale(link.href) && 'bg-primary/[0.05] text-primary'
                   } flex items-center gap-2 rounded-lg p-2 text-sm hover:bg-primary/[0.05]`}
                 >
                   {item}
                 </Link>
               ) : (
                 <button
-                  className="flex w-full items-center justify-start gap-2 rounded-lg p-2 text-sm hover:bg-primary/[0.05]"
-                  style={{ fontWeight: childrenHrefs.includes(pathname) ? 'bold' : 'normal' }}
+                  className={`flex w-full items-center justify-start gap-2 rounded-lg p-2 text-sm ${childrenHrefs.includes(pathname) ? 'font-semibold' : 'font-medium'} hover:bg-primary/[0.05]`}
                   onClick={() => setOpenItems((prev) => ({ ...prev, [link.name]: !prev[link.name] }))}
                 >
                   {item}
@@ -107,15 +108,12 @@ export default function AppNav() {
               {openItems[link.name] &&
                 link.children?.map((child) => {
                   const ChildIcon = child.icon
+                  const isActive = pathname === withLocale(child.href)
                   return (
                     <Link
                       key={child.href}
                       href={child.href}
-                      // TODO: change the style to use tailwind classes
-                      style={{ margin: 0, padding: '4px 6px', paddingLeft: '1.2rem' }}
-                      className={`${
-                        pathname === child.href && 'bg-primary/[0.05] text-primary'
-                      } flex h-[36px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium hover:bg-primary/[0.05] md:flex-none md:justify-start md:p-2 md:px-3`}
+                      className={`flex h-[36px] items-center gap-2 rounded-md ${isActive && 'bg-primary/[0.05]'} px-1 py-[6px] ${isRtl ? 'pr-[1.2rem]' : 'pl-[1.2rem]'} text-sm font-medium hover:bg-primary/[0.05]`}
                     >
                       <ChildIcon className="w-6" />
                       <p className="hidden md:block">{child.name}</p>
